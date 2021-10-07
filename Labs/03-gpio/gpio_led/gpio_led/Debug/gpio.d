467f9c43bb70e52b5f3506233c006b1eacf38c42
student <student@PC-634.urel.feec.vutbr.cz>
2021-10-07 10:56:54 +0200

    přidání laboratoře 3

--- Labs/03-gpio/gpio_led/gpio_led/Debug/gpio.d
+++ Labs/03-gpio/gpio_led/gpio_led/Debug/gpio.d
@@ -1,12 +1,9 @@
-main.d main.o: .././main.c \
- c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+gpio.d gpio.o: .././gpio.c .././gpio.h \
+ c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  C:\APPZ\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom328p.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
@@ -14,21 +11,17 @@ main.d main.o: .././main.c \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
 
-c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+.././gpio.h:
+
+c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
 
 c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
 c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
 
 c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
-
-c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
-
-c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
-
-c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
-
-c:\appz\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
 
 C:\APPZ\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom328p.h:
 
--- Labs/03-gpio/gpio_led/gpio_led/main.c
+++ Labs/03-gpio/gpio_led/gpio_led/main.c
@@ -1,19 +1,19 @@
 /***********************************************************************
  * 
- * Alternately toggle two LEDs when a push button is pressed.
+ * Alternately toggle two LEDs when a push button is pressed. Use 
+ * functions from GPIO library.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2018-Present Tomas Fryza
+ * Copyright (c) 2019-Present Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  * 
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
-#define LED_GREEN      PB5     // AVR pin where green LED is connected
-#define LED_GREEN_BB   PC0     // BB = bread board
-#define BUTTON         PD7
-#define BLINK_DELAY    500
+#define LED_GREEN   PB5     // AVR pin where green LED is connected
+#define LED_BB      PC0
+#define BLINK_DELAY 500
 #ifndef F_CPU
 # define F_CPU 16000000     // CPU frequency in Hz required for delay
 #endif
@@ -21,48 +21,42 @@
 /* Includes ----------------------------------------------------------*/
 #include <util/delay.h>     // Functions for busy-wait delay loops
 #include <avr/io.h>         // AVR device-specific IO definitions
-#include <avr/sfr_defs.h>   // library we were told to include
+#include "gpio.h"           // GPIO library for AVR-GCC
 
-/* Functions ---------------------------------------------------------*/
+/* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle two LEDs when a push button is pressed.
+ * Purpose:  Toggle two LEDs when a push button is pressed. Functions 
+ *           from user-defined GPIO library is used.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
     // Green LED at port B
-    // Set pin as output in Data Direction Register...
-    DDRB = DDRB | (1<<LED_GREEN);
-    
-
-    // ...and turn LED off in Data Register
-    PORTB = PORTB & ~(1<<LED_GREEN);
-    
+    GPIO_config_output(&DDRB, LED_GREEN);
+    GPIO_write_low(&PORTB, LED_GREEN);
 
     // Configure the second LED at port C
-    DDRC =  DDRC  | (1<<LED_GREEN_BB);
-    PORTC = PORTC | (1<<LED_GREEN_BB);
-    
+    GPIO_config_output(&DDRC, LED_BB);
+    GPIO_write_low(&PORTC, LED_BB);
+    // "BB" stands for bread board
     // Configure Push button at port D and enable internal pull-up resistor
-    DDRD =  DDRD & ~(1<<BUTTON)
-    PORTD = PORTD | (1<<BUTTON);
-    
-    PORTC = PORTC & ~(1<<LED_GREEN_BB); // setting off led on bread board
+
+
     // Infinite loop
     while (1)
     {
-        // Pause several milliseconds 
-       PORTB = PORTB ^ (1<<LED_GREEN);
-       PORTC = PORTC ^ (1<<LED_GREEN_BB);
+        // WRITE YOUR CODE HERE
+
+                
+       /* GPIO_write_high(&PORTB, LED_GREEN); // unpractical possibility
         _delay_ms(BLINK_DELAY);
-        /*
-        // WRITE YOUR CODE HERE 
-        if(bit_is_clear(PIND, BUTTON))
-            {
-                loop_until_bit_is_set(reg_name, pin_num)
-            }
-            */
+        GPIO_write_low(&PORTB, LED_GREEN);
+        _delay_ms(BLINK_DELAY);
+       */
+        GPIO_toggle(&PORTC, LED_BB);
+        GPIO_toggle(&PORTB, LED_GREEN);
+        _delay_ms(BLINK_DELAY);
     }
 
     // Will never reach this
